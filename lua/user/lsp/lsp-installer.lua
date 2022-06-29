a92826a7c75bf1ed9b7c19cd42fde192572a6833
Sokun Chorn (The Blockchain Guy) <chornsokun@gmail.com>
2022-06-29 17:47:00 +1000

    disable conflict json formatting, typescript disable suggestions

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -42,7 +42,7 @@ for _, lsp in ipairs(servers) do
   options = {
     on_attach = function(client, bufnr)
       -- disable formatting capabilities
-      if (client.name == "tsserver") then
+      if (client.name == "tsserver" or client.name == "jsonls") then
         client.resolved_capabilities.document_formatting = false
         client.resolved_capabilities.document_range_formatting = false
         --client.resolved_capabilities.publish_diagnostics = false
--- lua/user/lsp/settings/tsserver.lua
+++ lua/user/lsp/settings/tsserver.lua
@@ -2,6 +2,7 @@ return {
   -- https://github.com/typescript-language-server/typescript-language-server#initializationoptions
   init_options = {
     preferences = {
+      disableSuggestions = true,
       includeCompletionsWithSnippetText = true,
       includeCompletionsForImportStatements = true,
     },
