b853d80dd6b8ca944bc6278616386b3d9c42b4e4
Sokun Chorn <chornsokun@gmail.com>
2022-07-02 20:33:53 +1000

    format on save

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -4,9 +4,11 @@ require "user.lsp.null-ls"
 require "user.lsp.lsp_signature"
 
 -- auto format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
+  group = augroup,
   callback = function()
-    vim.lsp.buf.formatting_sync(nil, 100)
+    vim.lsp.buf.formatting_sync(nil, 1000)
   end
 })
--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -5,28 +5,29 @@ end
 -- ref. https://www.reddit.com/r/neovim/comments/qckrnp/share_your_prettier_and_eslint_formatting_setup/
 local formatting = null_ls.builtins.formatting
 --local diagnostics = null_ls.builtins.diagnostics
---local code_actions = null_ls.builtins.code_actions
+-- local code_actions = null_ls.builtins.code_actions
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup {
   sources = {
     --diagnostics.eslint,
-    --code_actions.eslint,
+    -- code_actions.eslint,
     formatting.prettier.with {
       extra_filetypes = { "toml", "solidity" },
     }
   },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.formatting_sync(nil, 2000)
-        end
-      })
-    end
-  end
+  -- on_attach = function(client, bufnr)
+  --   if client.supports_method("textDocument/formatting") then
+  --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  --     vim.api.nvim_create_autocmd("BufWritePre", {
+  --       group = augroup,
+  --       buffer = bufnr,
+  --       callback = function()
+  --         print("null-ls")
+  --         vim.lsp.buf.formatting_sync(nil, 2000)
+  --       end
+  --     })
+  --   end
+  -- end
 }
