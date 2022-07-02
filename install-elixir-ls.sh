6258bcb3ca363936334eca77204964a3bdaebc17
Sokun Chorn <chornsokun@gmail.com>
2022-07-02 14:11:23 +1000

    elixir-lsp installation script

---
+++ install-elixir-ls.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ELIXIR_LSP_SRC="$HOME/.elixir-ls"
+if [ ! -d "$ELIXIR_LSP_SRC" ]; then
+  git clone --depth 1 https://github.com/elixir-lsp/elixir-ls $ELIXIR_LSP_SRC
+fi
+pushd $ELIXIR_LSP_SRC
+git pull
+mix clean && mix deps.get && mix compile
+
+ELIXIR_LSP_SERVER_DIR="$HOME/.local/share/nvim/lsp_servers/elixirls/elixir-ls"
+mkdir -p $ELIXIR_LSP_SERVER_DIR
+mix elixir_ls.release -o $ELIXIR_LSP_SERVER_DIR
+popd
