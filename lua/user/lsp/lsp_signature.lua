ffdfa6ecc66708e4465eda03828adb1e54448a3d
Sokun Chorn <chornsokun@gmail.com>
2022-07-05 21:22:14 +1000

    this one is better

--- lua/user/lsp/lsp_signature.lua
+++ lua/user/lsp/lsp_signature.lua
@@ -4,11 +4,12 @@ if not status_ok then
 end
 local config = {
   bind = true, -- This is mandatory, otherwise border config won't get registered.
-  auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
+  auto_close_after = 1, -- autoclose signature float win after x sec, disabled if nil.
   extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
-  floating_window_off_y = -1,
+  floating_window_above_cur_line = false,
   handler_opts = {
     border = "rounded"
-  }
+  },
+  toggle_key = '<C->'
 }
 signature.setup(config)
