f5fdfc6731b5eaba8831af6dee0f978d90100fdc
Sokun Chorn <chornsokun@gmail.com>
2022-07-13 19:46:56 +1000

    lsp_signature wrap = true

--- lua/user/lsp/lsp_signature.lua
+++ lua/user/lsp/lsp_signature.lua
@@ -7,6 +7,7 @@ local config = {
   auto_close_after = 1, -- autoclose signature float win after x sec, disabled if nil.
   extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
   floating_window_above_cur_line = false,
+  wrap = true, -- allow doc/signature text wrap inside floating_window
   handler_opts = {
     border = "rounded"
   },
