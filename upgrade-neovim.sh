9a8e6ab2b744b38dda4dc93d8b07c610558d1974
Sokun Chorn <chornsokun@gmail.com>
2023-01-12 18:43:10 +1100

    install new version

---
+++ upgrade-neovim.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+curl -sL https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz -o nvim-linux64.tar.gz 
+tar -xf nvim-linux64.tar.gz
+sudo cp -a nvim-linux64/* /usr/local/
+rm -rf nvim-linux64
+rm nvim-linux64.tar.gz
+
