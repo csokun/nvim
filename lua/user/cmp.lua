c0ba9d3a4da12cf5e2dcadaf495f8d286d1b799c
Sokun Chorn (The Blockchain Guy) <chornsokun@gmail.com>
2023-01-16 19:54:13 +1100

    nvim_cmp

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -22,10 +22,8 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-u>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
