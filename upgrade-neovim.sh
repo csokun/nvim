3f1fadaf2e9feb7a218e84f2d9c14f676743a39f
Sokun Chorn (The Blockchain Guy) <chornsokun@gmail.com>
2023-02-24 21:16:15 +1100

    t

--- upgrade-neovim.sh
+++ upgrade-neovim.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-curl -sL https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz -o nvim-linux64.tar.gz 
+curl -sL https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz -o nvim-linux64.tar.gz
 tar -xf nvim-linux64.tar.gz
-sudo cp -a nvim-linux64/* /usr/local/
+sudo cp -a nvim-linux64/* /usr/
 rm -rf nvim-linux64
 rm nvim-linux64.tar.gz
-
